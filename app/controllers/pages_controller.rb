6d3da91acb661b13bec391e1e9ee87a347a7c9d8
Duy Khoa <duykhoa12t@gmail.com>
2015-05-12 00:20:02 +0800

    implement page controller return the login account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
   def facebook
-    render json: { message: request.env['omniauth.auth'] }
+    account = Account.from_omniauth(omniauth_params)
+
+    render json: { message: account.inspect }
+  end
+
+  private
+
+  def omniauth_params
+    {
+      uid: omniauth_data.uid,
+      email: omniauth_data.info.email || default_email,
+      provider: "facebook"
+    }
+  end
+
+  def omniauth_data
+    request.env["omniauth.auth"]
+  end
+
+  def default_email
+    "#{SecureRandom.hex(4)}@email.com"
   end
 end
---
+++ spec/controllers/pages_controllers_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe PagesController do
+
+  describe "GET facebook" do
+    context "new account registered" do
+      pending "TODO"
+    end
+
+    context "new account registered" do
+      pending "TODO"
+    end
+  end
+end

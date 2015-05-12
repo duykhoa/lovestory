cfa7babaad746e2808e610d00a3c68ebe559c2f9
Duy Khoa <duykhoa12t@gmail.com>
2015-05-12 23:27:41 +0800

    create a new page for first time signed in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,18 @@ class User < ActiveRecord::Base
   attr_accessor :uid, :provider
 
   has_many :accounts
+  has_many :pages
 
   after_create :create_related_account
+  after_create :create_page
 
   private
 
   def create_related_account
     accounts.create(provider: provider, uid: uid)
+  end
+
+  def create_page
+    pages.create
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,23 @@ require "rails_helper"
 
 describe User do
   it { should have_many(:accounts) }
+  it { should have_many(:pages) }
+
+  describe "create new page after create new user" do
+    before do
+      expect(User.count).to eq 0
+      expect(Account.count).to eq 0
+      expect(Page.count).to eq 0
+    end
+
+    it "creates new user" do
+      create(:user)
+      expect(User.count).to eq 1
+    end
+
+    after do
+      expect(Account.count).to eq 1
+      expect(Page.count).to eq 1
+    end
+  end
 end

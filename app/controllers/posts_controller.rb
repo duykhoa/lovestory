448b9a91b20e3320de06f5e2ad94aaa085c09b2e
Duy Khoa <duykhoa12t@gmail.com>
2015-05-16 18:16:45 +0800

    add crud action for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: %i(edit update destroy)
 
   def new
     @post = Post.new
@@ -11,16 +12,30 @@ class PostsController < ApplicationController
     redirect_to love_page
   end
 
-  def update
-  end
-
   def edit
   end
 
+  def update
+    @post.update(post_params)
+
+    redirect_to love_page
+  end
+
   def destroy
+    @post.update(post_params)
+
+    redirect_to love_page
   end
 
   private
+
+  def set_post
+    @post = Post.find_by_id post_id
+  end
+
+  def post_id
+    params.permit(:id)
+  end
 
   def love_page
     current_user.love_page

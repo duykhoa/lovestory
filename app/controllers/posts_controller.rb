1e07dc70407bf4cc41fb636b476c8f499ff05877
Duy Khoa <duykhoa12t@gmail.com>
2015-05-16 18:03:07 +0800

    add code to create post form controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @post = Post.new
   end
 
   def create
+    love_page.posts.create(post_params)
+
+    redirect_to love_page
   end
 
   def update
@@ -13,5 +18,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def love_page
+    current_user.love_page
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end

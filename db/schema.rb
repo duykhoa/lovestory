429b0bb90f4a057ec3dcdf680704797adac5fa03
Duy Khoa <duykhoa12t@gmail.com>
2015-05-23 12:34:09 +0800

    enable uuid-ossp extension in postgres and create invitations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523035348) do
+ActiveRecord::Schema.define(version: 20150523041511) do
 
-# Could not dump table "invitations" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "invitations", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
+    t.integer  "page_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "love_pages", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,10 +54,9 @@ ActiveRecord::Schema.define(version: 20150523035348) do
     t.string   "name"
     t.string   "uid"
     t.string   "provider"
-    t.string   "fb_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

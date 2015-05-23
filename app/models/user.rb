6c7378e5f1ac23e989d091f135f13c2479df3c67
Duy Khoa <duykhoa12t@gmail.com>
2015-05-23 13:54:16 +0800

    improve: remove some unused devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, omniauth_providers: [:facebook]
 

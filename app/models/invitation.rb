c1a8f2b55a67c5622755cae24d961bc5413baa6d
Duy Khoa <duykhoa12t@gmail.com>
2015-05-23 13:24:45 +0800

    add invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,5 @@
 class Invitation < ActiveRecord::Base
+  EXPIRED_IN = 12.hours
+
+  default_scope { where { created_at.gt EXPIRED_IN.ago } }
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Invitation do
+  describe ".find invitation by uuid" do
+    subject { Invitation.find_by_id(id) }
+
+    context "a invitation that exist" do
+      let!(:invitation) { create(:invitation) }
+      let(:id) { invitation.id }
+
+      it "returns invitation" do
+        is_expected.to eq invitation
+      end
+    end
+
+    context "a invitation that doesn't exist" do
+      let(:id) { 999 }
+      it "returns invitation" do
+        is_expected.to be nil
+      end
+    end
+
+    context "a invitation that expired" do
+      let!(:invitation) { create(:invitation, created_at: (Invitation::EXPIRED_IN + 1).day.ago) }
+      let(:id) { invitation.id }
+
+      it "returns invitation" do
+        is_expected.to be nil
+      end
+    end
+  end
 end

a32d2ad568e118cd0fa77fec363d3dbe32c9836d
Duy Khoa <duykhoa12t@gmail.com>
2015-05-31 21:08:21 +0800

    fix: wrong migration type

--- db/migrate/20150531021848_create_user_love_pages.rb
+++ db/migrate/20150531021848_create_user_love_pages.rb
@@ -2,7 +2,7 @@ class CreateUserLovePages < ActiveRecord::Migration
   def change
     create_table :user_love_pages do |t|
       t.integer :user_id
-      t.uuid :love_page_id
+      t.integer :love_page_id
     end
   end
 end

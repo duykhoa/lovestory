31002a21fad097073b81bca51aa4bdcd44023768
Duy Khoa <duykhoa12t@gmail.com>
2015-07-13 23:57:16 +0800

    wip: capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,28 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'lovestory'
+set :repo_url, 'git@github.com:duykhoa/lovestory.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/deploy/lovestory'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 # set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 2
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -36,5 +36,4 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
-
 end

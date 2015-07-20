db999e4041655ffd65ebf0feb77c20aca84b3e53
Duy Khoa <duykhoa12t@gmail.com>
2015-07-21 00:14:31 +0800

    feat: add .remove_expired_link method for Invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,7 +2,12 @@ class Invitation < ActiveRecord::Base
   EXPIRED_IN = 12.hours
 
   default_scope { where { created_at.gt EXPIRED_IN.ago } }
+  scope :expired, -> { where { created_at.lteq EXPIRED_IN.ago } }
 
   belongs_to :user
   belongs_to :love_page
+
+  def self.remove_expired_link
+    expired.destroy_all
+  end
 end

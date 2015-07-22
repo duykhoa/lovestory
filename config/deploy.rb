67fac13dcf8eb741a0ac3df5b662da2a2529b45d
Duy Khoa <duykhoa12t@gmail.com>
2015-07-23 01:01:26 +0800

    feat: remove unused comments in capistrano script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,10 @@
 set :application, 'lovestory'
 set :repo_url, 'git@github.com:duykhoa/lovestory.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
 set :deploy_to, '/home/deploy/lovestory'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
 
 set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 2
@@ -29,10 +21,6 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 

b73148ebe718447e5c07769cc8e83ef07ac53a7b
Duy Khoa <duykhoa12t@gmail.com>
2015-07-27 23:43:37 +0800

    feat: add model asset and attach photo to asset

---
+++ app/models/asset.rb
@@ -0,0 +1,4 @@
+class Asset < ActiveRecord::Base
+  has_attached_file :photo, :styles => { origin: "400x300^" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+end

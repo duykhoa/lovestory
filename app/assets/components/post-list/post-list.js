ea4270ad8d519c32f9d6de6213697ffe7ad6cedb
Duy Khoa <duykhoa12t@gmail.com>
2015-08-03 20:01:46 +0800

    feat: show Facebook avatar and post's created_at time

--- app/assets/components/post-list/post-list.js
+++ app/assets/components/post-list/post-list.js
@@ -6,11 +6,16 @@ Polymer({
       url: "/love_pages/1.json",
       success: function(data) {
         currentObject.data = data;
-        console.log(data);
       }
     })
   },
   postShowUrl: function(id) {
     return "/posts/" + id
+  },
+  fbImage: function(uid) {
+    return "https://graph.facebook.com/" + uid + "/picture"
+  },
+  timeAgoValue: function(time) {
+    return $.timeago(time)
   }
 });
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,8 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :fb_uid, :created_at
   has_many :assets
 
   def assets
     object.assets.map { |asset| asset.photo.url }
+  end
+
+  def fb_uid
+    object.user.uid
   end
 end

f51bce56186c7faef0a54cf4a788ad633959d483
Duy Khoa <duykhoa12t@gmail.com>
2015-08-23 15:24:36 +0800

    :smile: typo fixing in assets' type

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :photo, :styles => { origin: "600x400^" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { original: "600x400^" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :post

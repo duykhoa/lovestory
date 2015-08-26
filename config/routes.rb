4362e2921bba08044d3b1524b7f2ec8538ea1a99
Duy Khoa <duykhoa12t@gmail.com>
2015-08-26 10:14:43 +0800

    Using nestest routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 
   resources :love_pages, only: [:show, :index] do
     resources :invitations, only: [:create]
+    resources :posts, only: %i(new create show edit update destroy)
   end
 
   resources :invitations, only: [:show]
-
-  resources :posts, only: %i(new create show edit update destroy)
 
   root to: 'homes#index'
 end

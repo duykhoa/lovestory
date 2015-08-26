c2de16bd2aed19c085da0a666fe53c7de9bbed00
Duy Khoa <duykhoa12t@gmail.com>
2015-08-26 10:25:54 +0800

    Correct post path in postlist html

--- app/assets/components/post-list/post-list.js
+++ app/assets/components/post-list/post-list.js
@@ -19,7 +19,7 @@ Polymer({
     })
   },
   postShowUrl: function(id) {
-    return "/posts/" + id
+    return "/love_pages/" + this.pageId +"/posts/" + id
   },
   fbImage: function(uid) {
     return "https://graph.facebook.com/" + uid + "/picture"
@@ -28,10 +28,10 @@ Polymer({
     return $.timeago(time)
   },
   editLink: function(id) {
-    return "/posts/" + id + "/edit"
+    return "/love_pages/" + this.pageId +"/posts/" + id + "/edit"
   },
   deleteLink: function(id) {
-    return "/posts/" + id
+    return "/love_pages/" + this.pageId +"/posts/" + id
   },
   author: function(item) {
     return item.user_id == this.currentUserId

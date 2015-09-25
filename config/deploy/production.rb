dcdc512f1593a4df37835e49c3d1f141d978c7db
Duy Khoa <duykhoa12t@gmail.com>
2015-09-25 11:54:42 +0800

    update production deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :deploy_to, '/home/deploy/lovestory_production'
 
 set :rvm_ruby_version, '2.2.2@lovestory.space_production'
 
-server 'deploy.tenluaweb.com', user: 'deploy', roles: %w{web app db}
+server 'deploy.lovestory.space', user: 'deploy', roles: %w{web app db}

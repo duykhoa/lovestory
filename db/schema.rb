9334011b3ce3045c5240264726bacee0a5145dbe
Duy Khoa <duykhoa12t@gmail.com>
2015-10-17 19:27:44 +0800

    Add friendly_id gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012140512) do
+ActiveRecord::Schema.define(version: 20151017112229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 20151012140512) do
     t.uuid     "post_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "invitations", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.integer  "love_page_id"
     t.integer  "user_id"
@@ -39,7 +52,10 @@ ActiveRecord::Schema.define(version: 20151012140512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "slug"
   end
+
+  add_index "love_pages", ["slug"], name: "index_love_pages_on_slug", unique: true, using: :btree
 
   create_table "posts", id: :uuid, default: "uuid_generate_v1()", force: :cascade do |t|
     t.string   "title"

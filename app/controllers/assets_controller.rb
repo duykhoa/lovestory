ab53bc62371223cb2752b65abe3f21b02e878852
Duy Khoa <duykhoa12t@gmail.com>
2015-12-09 17:25:43 +0800

    Revert "Add upload element"
    This reverts commit 079087629a669c4e0f51f18a00ee285da3dea580.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,8 +1,6 @@
 class AssetsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def create
-    asset = Asset.new(photo: asset_params.tempfile)
+    asset = Asset.new(asset_params)
     asset.save!
 
     render json: { id: asset.id, photo_url: asset.photo.url }
@@ -11,6 +9,6 @@ class AssetsController < ApplicationController
   private
 
   def asset_params
-    params[:file]
+    params.require(:asset).permit(:photo)
   end
 end

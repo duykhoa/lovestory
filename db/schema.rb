22abf2f906a61e502307ad05eafbe774e2fe5efb
Duy Khoa <duykhoa12t@gmail.com>
2016-01-23 15:47:24 +0800

    Add deleted_at column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017112229) do
+ActiveRecord::Schema.define(version: 20160123074638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20151017112229) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
+    t.datetime "deleted_at"
   end
 
   create_table "user_love_pages", force: :cascade do |t|

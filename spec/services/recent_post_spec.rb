63b304fdf378d5c198152321dfb5b047c89bf07b
Duy Khoa <duykhoa12t@gmail.com>
2016-02-09 11:42:32 +0700

    Add test

--- spec/services/recent_post_spec.rb
+++ spec/services/recent_post_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe RecentPost do
   describe "#page" do
+    let!(:posts) { create_list(:post, 26, love_page: love_page) }
     let(:love_page) { create(:love_page) }
     let(:recent_post) { RecentPost.new(love_page) }
-    subject { recent_post.page }
+    subject { recent_post.page.count }
 
     it do
-      is_expected.to be_a(Array)
+      expect(recent_post.page(1).count).to eq 25
+      expect(recent_post.page(2).count).to eq 1
     end
   end
 end
